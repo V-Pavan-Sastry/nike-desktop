88c2d9a3ef567b6fc58744762861f30f64900bea
V PAVAN <you@example.com>
2023-07-28 20:55:37 +0530

    first:comit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navigation from './components/Navigation'  
+import Hero from './components/Hero'  
+const App =()=>{
+  return <div>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <Navigation />
+    <Hero/>
+  </div>
+}
+
+export default App;
